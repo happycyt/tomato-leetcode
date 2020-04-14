c7bed1e926fd87dd1e073b834878e11ca18afce6
caoyintong <yintong.cyt@dtwave-inc.com>
2020-04-14 09:53:55 +0800

    test

---
+++ src/main/java/com/tomato/leetcode/Demo1.java
@@ -0,0 +1,15 @@
+package com.tomato.leetcode;
+
+/**
+ * Description:
+ *
+ * @author: caoyintong
+ * Version: 1.0
+ * Create Date Time: 2020-04-14 09:52.
+ * Update Date Time:
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+
+    }
+}
